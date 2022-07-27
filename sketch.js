86413b55700408597c96c284568b78c6ae5e2402
Raffaele Amietta <70523797+malnatt@users.noreply.github.com>
2022-07-27 16:10:33 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@
 });*/
 
 function fetchData() {
-	console.log('start fetch');
+	fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.49271953641282&lon=9.299907172473253&appid=66ef0d49da67f2ff6856305249a75cdd");
 }
 
 fetchData();
