5176d4380f144b3fff550f6e5bb746f389243c29
Raffaele Amietta <70523797+malnatt@users.noreply.github.com>
2022-07-27 15:25:55 +0200

    weather api call 1

---
+++ sketch.js
@@ -0,0 +1,4 @@
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=45.49271953641282&lon=9.299907172473253&appid=66ef0d49da67f2ff6856305249a75cdd', 
+	function(data) {
+		console.log(data);
+});
