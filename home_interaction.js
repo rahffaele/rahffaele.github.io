decdf950913c50431fd10112c84bfb7da0c3e5b4
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2022-07-29 15:58:53 +0200

    Update home_interaction.js

--- home_interaction.js
+++ home_interaction.js
@@ -12,9 +12,9 @@ function setup() {
 }
 
 function draw() {
-    background(0, 0);
+    background(0);
     // PGraphics
-    pg.background(0, 0);
+    //pg.background(0, 0);
     pg.fill(255);
     pg.textFont("Helvetica");
     pg.textSize(130);
