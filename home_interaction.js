f783d25acdb49249b04289d532af7b0473d4047f
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2022-08-23 15:52:10 +0200

    Update home_interaction.js

--- home_interaction.js
+++ home_interaction.js
@@ -214,7 +214,7 @@ function draw() {
       pop();
       
       counter++
-      if (counter == voice1.length-1) counter = voice1.length-1;
+      if (counter == smile.length-1) counter = smile.length-1;
 
 
       x = x + cos(angle) * stepSize;
