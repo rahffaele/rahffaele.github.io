76a6b9d03015b71effbed70e15dc254182d62221
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2022-09-01 10:51:03 +0200

    Update itsgonnarain.js

--- itsgonnarain.js
+++ itsgonnarain.js
@@ -1,4 +1,4 @@
-fetch('itsgonnarain.mp3')
+fetch('../resource/bass.mp3')
   .then(response => response.arrayBuffer())
   .then(arrayBuffer => console.log('Received', arrayBuffer))
   .catch(e => console.error(e));
