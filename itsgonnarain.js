95cae444d20476abb6d712191320128f65605ce5
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2022-09-01 12:53:02 +0200

    Update itsgonnarain.js

--- itsgonnarain.js
+++ itsgonnarain.js
@@ -1,4 +1,12 @@
+let audioContext = new AudioContext();
+
 fetch('../resource/bass.mp3')
   .then(response => response.arrayBuffer())
-  .then(arrayBuffer => console.log('Received', arrayBuffer))
+  .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+  .then(audioBuffer => {
+    let sourceNode = audioContext.createBufferSource();
+    sourceNode.buffer = audioBuffer;
+    sourceNode.connect(audioContext.destination);
+    sourceNode.start();
+  })
   .catch(e => console.error(e));
