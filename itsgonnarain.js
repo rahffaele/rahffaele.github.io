305c913e6cef7238d9f19fe57054f47b3f7c797c
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2022-10-16 19:15:40 +0200

    Update itsgonnarain.js

--- itsgonnarain.js
+++ itsgonnarain.js
@@ -1,3 +1,38 @@
+const sample_library = {
+  'WARM PAD' : [
+    {note: 'A#', octave: 4, file: "../samples/Warm pad/Warm pad A#4.wav"},
+    {note: 'A', octave: 4, file: "../samples/Warm pad/Warm pad A4.wav"},
+    {note: 'B', octave: 4, file: "../samples/Warm pad/Warm pad B4.wav"},
+    {note: 'C#', octave: 4, file: "../samples/Warm pad/Warm pad C#4.wav"},
+    {note: 'C', octave: 4, file: "../samples/Warm pad/Warm pad C4.wav"},
+    {note: 'D#', octave: 4, file: "../samples/Warm pad/Warm pad D#4.wav"},
+    {note: 'D', octave: 4, file: "../samples/Warm pad/Warm pad D4.wav"},
+    {note: 'E', octave: 4, file: "../samples/Warm pad/Warm pad E4.wav"},
+    {note: 'F#', octave: 4, file: "../samples/Warm pad/Warm pad F#4.wav"},
+    {note: 'F', octave: 4, file: "../samples/Warm pad/Warm pad F4.wav"},
+    {note: 'G#', octave: 4, file: "../samples/Warm pad/Warm pad G#4.wav"},
+    {note: 'G', octave: 4, file: "../samples/Warm pad/Warm pad G4.wav"},
+    {note: 'C', octave: 5, file: "../samples/Warm pad/Warm pad C5.wav"},
+  ]
+
+  'SYNTH BASS' : [
+    {note: 'A#', octave: 2, file: "../samples/Synth Bass/Synth Bass A#2.wav"},
+    {note: 'A', octave: 2, file: "../samples/Synth Bass/Synth Bass A2.wav"},
+    {note: 'B', octave: 2, file: "../samples/Synth Bass/Synth Bass B2.wav"},
+    {note: 'C#', octave: 2, file: "../samples/Synth Bass/Synth Bass C#2.wav"},
+    {note: 'C', octave: 2, file: "../samples/Synth Bass/Synth Bass C2.wav"},
+    {note: 'D#', octave: 2, file: "../samples/Synth Bass/Synth Bass D#2.wav"},
+    {note: 'D', octave: 2, file: "../samples/Synth Bass/Synth Bass D2.wav"},
+    {note: 'E', octave: 2, file: "../samples/Synth Bass/Synth Bass E2.wav"},
+    {note: 'F#', octave: 2, file: "../samples/Synth Bass/Synth Bass F#2.wav"},
+    {note: 'F', octave: 2, file: "../samples/Synth Bass/Synth Bass F2.wav"},
+    {note: 'G#', octave: 2, file: "../samples/Synth Bass/Synth Bass G#2.wav"},
+    {note: 'G', octave: 2, file: "../samples/Synth Bass/Synth Bass G2.wav"},
+    {note: 'C', octave: 3, file: "../samples/Synth Bass/Synth Bass C3.wav"},
+  ]
+};
+
+
 function play(){
 
 let audioContext = new AudioContext();
