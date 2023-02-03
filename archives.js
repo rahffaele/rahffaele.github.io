b51110d731fd661a0b6e172eba5ce314052e312a
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-02-03 19:05:13 +0100

    Update archives.js

--- archives.js
+++ archives.js
@@ -1,22 +1,7 @@
 var card = document.getElementsByClassName("archive_card");
-var newq;
-let h,w,nh,nw,s; 
 
-function newPosition(){   
-  h = window.innerHeight - 50;
-  w = window.innerWidth - 50;
-  nh = Math.floor(Math.random() * h);
-  nw = Math.floor(Math.random() * w);
-  s = Math.floor(Math.random()*1000) + 500
-  return [nh,nw,s];       
-}
+var posx = getRandomNumber(); 
+var posy = getRandomNumber(); 
 
-card.forEach(function card(myclass) {
-  var newq = newPosition();
-  $(myclass).animate({ 
-    top: nh, left: nw; 
-    },
-    newq[2],   function(){
-    card(myclass);        
-  });
-});
+card.style.left = posx;
+card.style.top = posy;
