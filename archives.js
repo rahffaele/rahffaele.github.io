a319032fa713dd40c95a0da0004a76157b5cbed1
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-02-06 15:14:06 +0100

    Update archives.js

--- archives.js
+++ archives.js
@@ -44,18 +44,18 @@ archiveCards.forEach(card => {
     const randomLeft = getRandomNumber(0, window.innerWidth - card.offsetWidth);
     
     // set the generated random values as top and left positions for the card
-    for (var i = 0; i < cards.length; i++) {
-    cards[i].style.left = window.innerWidth + 'px';
-    cards[i].style.top = window.innerHeight + 'px';
+    for (var i = 0; i < archiveCards.length; i++) {
+    archiveCards[i].style.left = window.innerWidth + 'px';
+    archiveCards[i].style.top = window.innerHeight + 'px';
   }
 
   // wait for a short time to let the initial positions take effect
   setTimeout(function() {
     // set the final random positions of the cards
-    for (var i = 0; i < cards.length; i++) {
-      cards[i].style.left = randomX() + 'px';
-      cards[i].style.top = randomY() + 'px';
-      cards[i].style.opacity = 1;
+    for (var i = 0; i < archiveCards.length; i++) {
+      archiveCards[i].style.left = randomX() + 'px';
+      archiveCards[i].style.top = randomY() + 'px';
+      archiveCards[i].style.opacity = 1;
     }
   }, 100);
     
