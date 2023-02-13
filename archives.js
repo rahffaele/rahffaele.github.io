292d1b0178089a5ae3e4cbc609093e988ef8c6df
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-02-13 12:45:10 +0100

    Update archives.js

--- archives.js
+++ archives.js
@@ -10,23 +10,28 @@ function getRandomNumber(min, max) {
 }
 
 // function to check if a card element overlaps with any other card
-ffunction checkOverlap(card) {
+function checkOverlap(card) {
   let overlapping = false;
-
-  cards.forEach(otherCard => {
+  
+  // loop through each card element
+  archiveCards.forEach(otherCard => {
+    // check if the card is the same as the one being passed to the function
     if (card === otherCard) return;
-
+    
+    // get the bounding rectangles of the card and otherCard
     const cardRect = card.getBoundingClientRect();
     const otherCardRect = otherCard.getBoundingClientRect();
-
-    overlapping = !(cardRect.right <= otherCardRect.left || 
-                   cardRect.left >= otherCardRect.right || 
-                   cardRect.bottom <= otherCardRect.top || 
-                   cardRect.top >= otherCardRect.bottom);
-
+    
+    // check if the two rectangles overlap
+    overlapping = !(cardRect.right < otherCardRect.left || 
+                   cardRect.left > otherCardRect.right || 
+                   cardRect.bottom < otherCardRect.top || 
+                   cardRect.top > otherCardRect.bottom);
+    
+    // if overlapping, stop checking for overlapping with other cards
     if (overlapping) return;
   });
-
+  
   return overlapping;
 }
 
@@ -37,8 +42,8 @@ archiveCards.forEach(card => {
   // repeat until no overlapping occurs
   while (overlapping) {
     // generate random values for top and left positions
-    const randomTop = getRandomNumber(0, window.innerHeight - card.offsetHeight +300);
-    const randomLeft = getRandomNumber(300, window.innerWidth - card.offsetWidth +300);
+    const randomTop = getRandomNumber(0, window.innerHeight - card.offsetHeight);
+    const randomLeft = getRandomNumber(300, window.innerWidth - card.offsetWidth);
     
     // set the generated random values as top and left positions for the card
     card.style.top = `${randomTop}px`;
