a8343df9378fa6fa41cf7891d4a8bd266fbf0bda
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-02-18 22:58:47 +0100

    cover portfolio hover function

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#first_project").hover(function(){
+        $("#cxmap-cover").removeClass("hide");
+    }, function(){
+        $("#cxmap-cover").addClass("hide");
+    });
+});
