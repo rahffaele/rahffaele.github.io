3c8352f0e9d07f849c8d1c5441c62091364da4eb
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-02-19 21:18:43 +0100

    Update archive.js

--- archive.js
+++ archive.js
@@ -1,8 +1,12 @@
-const text = document.getElementById('blurred-text');
+document.addEventListener("DOMContentLoaded", function() {
+  const text = document.getElementById('blurred-text');
 
-text.addEventListener('mousemove', function(e) {
-  const x = e.clientX - this.offsetLeft;
-  const y = e.clientY - this.offsetTop;
-  this.style.setProperty('--x', `${ x }px`);
-  this.style.setProperty('--y', `${ y }px`);
+  text.addEventListener('mousemove', function(e) {
+    const x = e.clientX - this.offsetLeft;
+    const y = e.clientY - this.offsetTop;
+    this.style.setProperty('--x', `${ x }px`);
+    this.style.setProperty('--y', `${ y }px`);
+  });
 });
+
+
