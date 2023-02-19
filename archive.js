68cc97571a1d7d39095f89f43a53de2af4936db8
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-02-19 21:13:17 +0100

    archive.js new +. archive.css new

---
+++ archive.js
@@ -0,0 +1,8 @@
+const text = document.getElementById('blurred-text');
+
+text.addEventListener('mousemove', function(e) {
+  const x = e.clientX - this.offsetLeft;
+  const y = e.clientY - this.offsetTop;
+  this.style.setProperty('--x', `${ x }px`);
+  this.style.setProperty('--y', `${ y }px`);
+});
