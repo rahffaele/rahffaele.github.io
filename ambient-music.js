ea79391b68639780c66bd7fbb01fa13a166a3646
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-03 15:29:01 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -1,6 +1,8 @@
 function play(){
 
-let synth = new Tone.MonoSynth();
+let synth = new Tone.MonoSynth({
+  oscillator: {type: 'sawtooth'}
+});
 synth.toMaster();
 synth.triggerAttackRelease('C4', 1);
 }
