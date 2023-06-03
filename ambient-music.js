3fbe32477049e5dc7a3c062480488ae84655c28c
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-03 19:17:27 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -105,7 +105,7 @@ new Tone.Loop(time => {
 
   // Trigger G4 after 23 measures + two 1/4 notes. Hold for a half note.
   synthTwo.triggerAttackRelease('C2', '0:2', '+23:2');
-}, '30m').start();
+}, '40m').start();
 
 
 Tone.Transport.start();
