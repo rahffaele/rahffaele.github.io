6df96f5a7317645af1ebfd6474c23025aa69d41c
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-03 15:47:25 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -24,7 +24,7 @@ let synth = new Tone.DuoSynth({
       releaseCurve: 'linear'
     },
     filterEnvelope: {
-      baseFrequency: 200,
+      baseFrequency: 150,
       octaves: 1,
       attack: 0,
       decay: 0,
