e61e6f3a8d6369e57d7811dfd0684f48f4deb054
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-03 19:31:48 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -103,9 +103,9 @@ new Tone.Loop(time => {
   synthThree.setNote('C2', '3:0', '+3.0');
 
   // Trigger B3 after 11 measures + two 1/4 notes + two 1/16 notes. Hold for one measure
-  synthThree.triggerAttackRelease('B3', '1m', '+6:2:2');
+  synthThree.triggerAttackRelease('B3', '1m', '+2:2:2');
   // Switch to G3 after a 1/2 note more
-  synthThree.setNote('E2', '+12:0:2');
+  synthThree.setNote('E2', '+3:0:2');
 
   // Trigger G4 after 23 measures + two 1/4 notes. Hold for a half note.
   synthThree.triggerAttackRelease('C2', '0:2', '+23:2');
