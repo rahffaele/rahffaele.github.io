a5bacd5a2be52bc1142f72777fd41c5547b916ea
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-03 18:08:27 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -38,11 +38,17 @@ let leftPanner = new Tone.Panner(-0.5); // No longer connected to master!
 let rightPanner = new Tone.Panner(0.5); // No longer connected to master!
 let echo = new Tone.FeedbackDelay('16n', 0.5);
 
+let delay = Tone.context.createDelay(6.0); // Borrow the AudioContext from Tone.js
+delay.delayTime.value = 6.0;
+
+
 synthOne.connect(leftPanner);
 synthTwo.connect(rightPanner);
 leftPanner.connect(echo);
 rightPanner.connect(echo);
 echo.toMaster();
+echo.connect(delay);
+delay.connect(Tone.context.destination);
 
 new Tone.Loop(time => {
     // Trigger C5, and hold for a full note (measure) + two 1/4 notes
