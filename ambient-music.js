5897bf21f041208563ee6a6846156c4f30b7e70d
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-03 15:35:20 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -4,7 +4,9 @@ let synth = new Tone.MonoSynth({
   oscillator: {type: 'sawtooth'},
   filterEnvelope: {
     baseFrequency: 200,
-    octaves: 1
+    octaves: 1,
+    attack: 0,
+    decay: 0
   }
 });
 synth.toMaster();
