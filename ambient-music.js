cbb4283f6d4e14c090880259c1a24eb53c49b031
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-03 15:47:13 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -2,21 +2,6 @@ function play(){
 
 let synth = new Tone.DuoSynth({
   voice0: {
-    oscillator: {type: 'sine'},
-    envelope: {
-      attack: 0.1,
-      release: 4,
-      releaseCurve: 'linear'
-    },
-    filterEnvelope: {
-      baseFrequency: 200,
-      octaves: 2,
-      attack: 0,
-      decay: 0,
-      release: 1000
-    }
-  },
-  voice1: {
     oscillator: {type: 'sawtooth'},
     envelope: {
       attack: 0.1,
@@ -25,7 +10,22 @@ let synth = new Tone.DuoSynth({
     },
     filterEnvelope: {
       baseFrequency: 200,
-      octaves: 2,
+      octaves: 1,
+      attack: 0,
+      decay: 0,
+      release: 1000
+    }
+  },
+  voice1: {
+    oscillator: {type: 'sine'},
+    envelope: {
+      attack: 0.1,
+      release: 4,
+      releaseCurve: 'linear'
+    },
+    filterEnvelope: {
+      baseFrequency: 200,
+      octaves: 1,
       attack: 0,
       decay: 0,
       release: 1000
