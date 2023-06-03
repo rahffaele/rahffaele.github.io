6c5c7387370bfbe58465fa9b92c966c6c09661db
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-03 18:34:58 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -38,7 +38,7 @@ let synthTwo = makeSynth();
 let leftPanner = new Tone.Panner(-0.5); // No longer connected to master!
 let rightPanner = new Tone.Panner(0.5); // No longer connected to master!
 let echo = new Tone.FeedbackDelay('16n', 0.2);
-let mainEcho = new Tone.FeedbackDelay('6m', 0.2);
+let mainEcho = new Tone.FeedbackDelay(6, 0.2);
 
 synthOne.connect(leftPanner);
 synthTwo.connect(rightPanner);
