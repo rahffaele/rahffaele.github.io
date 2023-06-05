79ebbe6cdb7be4d6c6035bb3f980c6b349957f36
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-05 18:36:06 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -52,7 +52,7 @@ function makeSynthTwo() {
     harmonicity: 4,
     volume: -20,
     voice0: {
-      oscillator: {type: 'sine'},
+      oscillator: {type: 'noise'},
       envelope,
       filterEnvelope
     },
