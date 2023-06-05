3ec367d3125aed2d10af6de63b336e4e6f4da8c7
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-06 01:31:37 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -9,7 +9,7 @@ async function play(){
     const city = selectedCity;
 
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=Milan&appid=49a5b64679cabaa392cc7fe6b5826a92&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=49a5b64679cabaa392cc7fe6b5826a92&units=metric`
     );
 
     console.log(response.data); //control
