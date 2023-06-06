932c1d9d85aaca0d8cb1a756401a2d2bc74ef77c
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-06 22:05:48 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -2,51 +2,6 @@ let isPlaying = false;
 let loopOne, loopTwo, loopThree, loopFour, loopHighMelody;
 let synthOne, synthTwo, synthThree, synthFour, highMelody;
 
-function lerpColor(startColor, endColor, t) {
-  // Perform linear interpolation for each RGB channel
-  const lerpedRGB = startColor.map((channel, i) =>
-    Math.round(channel + (endColor[i] - channel) * t)
-  );
-
-  // Return the interpolated RGB representation
-  return lerpedRGB;
-}
-
-function mapValueToColor(value, lowValue, highValue, lowColor, highColor) {
-  if (temp <= lowValue) {
-    return lowColor;
-  } else if (temp >= highValue) {
-    return highColor;
-  } else {
-    const t = (temp - lowValue) / (highValue - lowValue); // Normalize the value to the range [0, 1]
-    return lerpColor(lowColor, highColor, t);
-  }
-
-  if (usaqi <= lowValue) {
-    return lowColor;
-  } else if (usaqi >= highValue) {
-    return highColor;
-  } else {
-    const t = (usaqi - lowValue) / (highValue - lowValue); // Normalize the value to the range [0, 1]
-    return lerpColor(lowColor, highColor, t);
-  }
-}
-
-// Example usage
-const lowValue = 0;
-const highValue = 35;
-const lowTempColor = [83, 97, 125];
-const highTempColor = [0, 37, 114];
-const lowPollColor = [79, 147, 186];
-const highPollColor = [185, 185, 185];
-
-const tempColor = mapValueToColor(temp, lowValue, highValue, lowTempColor, highTempColor);
-const pollColor = mapValueToColor(usaqi, lowValue, highValue, lowPollColor, highPollColor);
-
-console.log('Temperature Color:', tempColor);
-console.log('Pollution Color:', pollColor);
-
-
 async function play() {
   if (isPlaying) {
     // Stop the music if it's already playing
@@ -260,6 +215,50 @@ async function play() {
 		} catch (error) {
       		console.error('Error fetching traffic data:', error);
     	}
+
+    	function lerpColor(startColor, endColor, t) {
+  // Perform linear interpolation for each RGB channel
+  const lerpedRGB = startColor.map((channel, i) =>
+    Math.round(channel + (endColor[i] - channel) * t)
+  );
+
+  // Return the interpolated RGB representation
+  return lerpedRGB;
+}
+
+function mapValueToColor(value, lowValue, highValue, lowColor, highColor) {
+  if (temp <= lowValue) {
+    return lowColor;
+  } else if (temp >= highValue) {
+    return highColor;
+  } else {
+    const t = (temp - lowValue) / (highValue - lowValue); // Normalize the value to the range [0, 1]
+    return lerpColor(lowColor, highColor, t);
+  }
+
+  if (usaqi <= lowValue) {
+    return lowColor;
+  } else if (usaqi >= highValue) {
+    return highColor;
+  } else {
+    const t = (usaqi - lowValue) / (highValue - lowValue); // Normalize the value to the range [0, 1]
+    return lerpColor(lowColor, highColor, t);
+  }
+}
+
+// Example usage
+const lowValue = 0;
+const highValue = 35;
+const lowTempColor = [83, 97, 125];
+const highTempColor = [0, 37, 114];
+const lowPollColor = [79, 147, 186];
+const highPollColor = [185, 185, 185];
+
+const tempColor = mapValueToColor(temp, lowValue, highValue, lowTempColor, highTempColor);
+const pollColor = mapValueToColor(usaqi, lowValue, highValue, lowPollColor, highPollColor);
+
+console.log('Temperature Color:', tempColor);
+console.log('Pollution Color:', pollColor);
 
     function makeSynthOne() {
       let envelope = {
