9d726f4755c2e64d62fc0756698fe1d1f201953f
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-06 16:45:38 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -63,7 +63,7 @@ async function play() {
     }
 
     try {
-    	const apiKeyTomTom = "RP964APHqo03cYONTkMwNI5DNGFtQNEA"
+    	const apiKeyTomTom = "RP964APHqo03cYONTkMwNI5DNGFtQNEA";
 			const responseTomTom = await axios.get(
         		`https://api.tomtom.com/map/1/tile/basic/main/10/4/8.pbf?key=${apiKeyTomTom}&language=en-US`
       		);
