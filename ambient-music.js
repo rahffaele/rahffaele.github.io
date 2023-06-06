92799bbc42c9f5400173d091cdfcee14761ace51
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-06 19:06:38 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -119,7 +119,7 @@ async function play() {
       	const city = selectedCity;
 
       	const response = await axios.get(
-        	`http://api.airvisual.com/v2/city?city=${city}&state=${cityState}&country=${cityCountry}&key=${apiKeyAir}`,
+        	//`http://api.airvisual.com/v2/city?city=${city}&state=${cityState}&country=${cityCountry}&key=${apiKeyAir}`,
         	`http://api.airvisual.com/v2/cities?state=${cityState}&country=${cityCountry}&key=${apiKeyAir}`,
         	//`http://api.airvisual.com/v2/states?country=${cityCountry}&key=${apiKeyAir}`,
       	);
