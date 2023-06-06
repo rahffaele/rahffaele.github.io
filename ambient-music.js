4b3f098c7c951b1d9199aa6d832ed6949bb02d35
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-06 23:20:04 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -416,7 +416,7 @@ async function play() {
     }
 
     const background = document.getElementById("bg");
-  	background.style.background= `linear-gradient(to bottom, ${tempColor}, ${pollColor});`;
+  	background.style.background-image = `linear-gradient(to bottom, ${tempColor}, ${pollColor})`;
 
     //Tone.Transport.bpm.value = 120;
 }
