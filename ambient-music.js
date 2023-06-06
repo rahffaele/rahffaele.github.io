1b0b269dc9eb2145e9def2568451775dc07bb3d7
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-07 01:12:13 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -27,6 +27,7 @@ async function play() {
     } else {
         const button = document.getElementById("startStop");
         button.textContent = "Pause";
+        button.click();
         isPlaying = true;
 
         try {
