6d6db9f32deb756108416e74991f176bd512ed8a
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-06-07 02:00:20 +0200

    Update ambient-music.js

--- ambient-music.js
+++ ambient-music.js
@@ -12,7 +12,7 @@ async function gradient(){
     const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=Milan&appid=${apiKeyTemp}&units=metric`
     );
-    const temp = response.data.main;
+    const temp = response.data.main.temp;
     console.log(response.data); 
     console.log("temp default:", temp)
     tempColorDefault = calculateTempColor(temp);
